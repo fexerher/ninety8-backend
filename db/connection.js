254cc8396307077dfec1a430d2cbbff4fb1acec7
Andrei <oscarxx8@hotmail.com>
2023-04-12 17:51:42 -0500

    port added to connection

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,7 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize('ninety8', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql',
+    port: 3306
     //logging: false
 })
 
